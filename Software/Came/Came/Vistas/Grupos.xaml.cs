543391ce1a2412f6ef3379a5541d4ebfc5bff988
jesus carlos cornejo rubio <jccornejor@hotmail.com>
2013-04-01 01:04:44 -0700

    primera version de grupos

--- Software/Came/Came/Vistas/Grupos.xaml.cs
+++ Software/Came/Came/Vistas/Grupos.xaml.cs
@@ -25,5 +25,163 @@ namespace Came.Vistas
         {
             InitializeComponent();
         }
+
+        #region Adm Grupos
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void verGrupoButton_Click(object sender, RoutedEventArgs e)
+        {
+            grupoActionsCanvas.Visibility = System.Windows.Visibility.Visible;
+            gruposGroupBox.Visibility = System.Windows.Visibility.Hidden;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void agregarGrupoButton_Click(object sender, RoutedEventArgs e)
+        {
+            grupoActionsCanvas.Visibility = System.Windows.Visibility.Visible;
+            gruposGroupBox.Visibility = System.Windows.Visibility.Hidden;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void actualizarGrupoButton_Click(object sender, RoutedEventArgs e)
+        {
+            grupoActionsCanvas.Visibility = System.Windows.Visibility.Visible;
+            gruposGroupBox.Visibility = System.Windows.Visibility.Hidden;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void eliminarGrupoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Acciones Grupo
+
+        #region Acciones de Maestros
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void verMaestroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void agregarMaestroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void eliminarMaestroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Acciones de Alumnos
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void verAlumnoButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void agregarAlumnoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void eliminarAlumnoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        } 
+
+        #endregion
+
+        #region Acciones de Grupo
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void modificarGrupoButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void crearGrupoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void volverButton_Click(object sender, RoutedEventArgs e)
+        {
+            grupoActionsCanvas.Visibility = System.Windows.Visibility.Hidden;
+            gruposGroupBox.Visibility = System.Windows.Visibility.Visible;
+        } 
+
+        #endregion
+        
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void volverMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
     }
 }
